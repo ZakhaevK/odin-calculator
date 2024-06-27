1c35351b20f256ac38e23149345b51b716cc96a8
ZakhaevK <zkupke@gmail.com>
2024-06-27 18:28:53 +1000

    setup basic updateDisplay() and operand buttons includes event listener for operand class buttons

--- calculator.js
+++ calculator.js
@@ -1,7 +1,37 @@
+// Elements for DOM manipulation
+const display = document.getElementById("display");
+const clearBut = document.getElementById("ac");
+const signBut = document.getElementById("+/-");
+const percentBut = document.getElementById("percentage");
+const operandButs = document.getElementsByClassName("operand");
+
+for (let button of operandButs) {
+  console.log(button.value);
+  button.addEventListener("click", () => updateDisplay(button.value));
+}
+
 // Variables for calculator operations
 let num1 = 0;
 let op = '';
 let num2 = 0;
+
+
+// Update display, handles decimal point
+function updateDisplay(string) {
+  console.log(string)
+  if (string == ".") {
+    if (!display.textContent.includes(".")) 
+      display.textContent += string;
+    return
+  }
+
+  if (display.textContent === "0") {
+    display.textContent = string;
+  } else {
+    display.textContent += string;
+  }
+}
+
 
 // Basic calculator operations
 function add(a, b) {
@@ -19,6 +49,7 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
 
 // Calls the calculator function
 function operate(op, num1, num2) {
